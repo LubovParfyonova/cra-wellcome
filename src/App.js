5756ea83c48c78ba6d6803b47fb28c98a62f2cc5
Lubov <lubaparfyonova@gmail.com>
2022-02-23 14:11:12 +0200

    Add button toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Greeting from './components/Greeting';
+import { Fragment } from 'react';
 
 function App() {
+
+const user = {
+  firstName: 'Vlad',
+  photo: 'https://www.shareicon.net/data/512x512/2016/10/25/847809_user_512x512.png'
+}
+const user2 = {
+  firstName: 'Masha',
+  photo: 'https://www.shareicon.net/data/512x512/2016/10/25/847809_user_512x512.png'
+}
+const user3 = {
+  firstName: 'Dasha',
+  photo: 'https://www.shareicon.net/data/512x512/2016/10/25/847809_user_512x512.png'
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  <>
+    <Greeting user = {user} isGreeting/>
+    <Greeting user = {user2}/>
+    <Greeting user = {user3} isGreeting/>
+  </>
+  )
 }
 
 export default App;
